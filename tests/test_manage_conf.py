328253bdbd3340a1c6b4415d3b21cc55ccbe2f6a
Sam Atkins <samatkins@outlook.com>
2019-03-29 09:15:34 +0000

    test: Add test for deserialised method

--- tests/test_manage_conf.py
+++ tests/test_manage_conf.py
@@ -1,5 +1,23 @@
+import pytest
+
 from manage_conf import __version__
+from manage_conf import RemoteSettings
+
+
+@pytest.fixture(scope="class")
+def remote_settings_class():
+    rs = RemoteSettings()
+    yield rs
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test__deserialise_method_json_loads(remote_settings_class):
+    name = "ALLOWED_HOSTS"
+    value = "\"['uglyurl.execute-api.us-east-1.amazonaws.com']\""
+    assert isinstance(value, str)
+    deserialised_value = remote_settings_class._deserialise(name, value)
+    assert isinstance(deserialised_value, str)
+    assert deserialised_value == "['uglyurl.execute-api.us-east-1.amazonaws.com']"
